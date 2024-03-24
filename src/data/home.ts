6fc33831a0ea3270a6fa984a4b51fe8c5d3030d4
anvarsaidovsh <bananoanimation12@gmail.com>
2024-03-24 21:07:03 +0500

    working on categories

---
+++ src/data/home.ts
@@ -0,0 +1,17 @@
+export const categories = [
+    'All',
+    'JavaScript',
+    'TypeScript',
+    'Golang',
+    'Axum',
+    'Nextjs',
+    'Laravel',
+    'OOPs',
+    'Functional Programming',
+    'Web Development',
+    'Rust',
+    'Music',
+    'Imperative programming',
+    'Low level Programming',
+    'High level programming'
+]
