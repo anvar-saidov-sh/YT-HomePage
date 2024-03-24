08fe51c4314d2324bc88a1db74a8dd4f1672a7d4
anvarsaidovsh <bananoanimation12@gmail.com>
2024-03-24 20:39:03 +0500

    working on sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,10 @@ function App() {
     <>
       <div className='max-h-screen flex flex-col'>
           <PageHeader/>
+          <div className='grid grid-cols-auto-[auto, 1fr] flex-grow-1 overflow-auto'>
+              <div>Sidebar</div>
+              <div></div>
+          </div>
       </div>
     </>
   )
