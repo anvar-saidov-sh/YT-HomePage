6a53a82fefbe267d71ff128274d962969fa32867
anvarsaidovsh <bananoanimation12@gmail.com>
2024-03-24 12:46:53 +0500

    Add button.tsx

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -1,9 +1,19 @@
-
+import { Menu } from 'lucide-react'
+import Logo from '../assets/react.svg'
 
 function PageHeader() {
   return (
     <>
-        
+        <div className="flex gap-10 lg:gap-20 justify-between">
+            <div className="flex gap-4 items-center flex-shrink-0">
+                <button><Menu/></button>
+                <a href="/">
+                    <img src={Logo} alt="Logo" className='h-6'/>
+                </a>
+            </div>
+            <div></div>
+            <div></div>
+        </div>
     </>
   )
 }
