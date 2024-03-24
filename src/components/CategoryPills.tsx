1c07c63f03ca386a875c84a2846c047fc468c019
anvarsaidovsh <bananoanimation12@gmail.com>
2024-03-24 21:48:36 +0500

    working on categories

--- src/components/CategoryPills.tsx
+++ src/components/CategoryPills.tsx
@@ -1,4 +1,6 @@
+import { ChevronLeft, ChevronRight } from "lucide-react"
 import Button from "./Button"
+import { useRef, useState } from "react"
 
 type CategoryPillsProps = {
     categories: string[],
@@ -6,15 +8,21 @@ type CategoryPillsProps = {
     onSelect: (category: string) => void
 }
 
+const TRANSLATE_AMOUNT = 200
+
 function CategoryPills({ categories, selectedCategory, onSelect }: CategoryPillsProps) {
+    const [translate, setTranslate] = useState(0)
+    const [isLeftVisible, setIsLeftVisible] = useState(false)
+    const [isRightVisible, setIsRightVisible] = useState(false)
+    const containerRef = useRef<HTMLDivElement>(null)
     return (
         <>
             <div className="overflow-x-hidden relative">
-                <div className="flex whitespace-nowrap gap-3 transition-transform w-[max-content]">
+                <div ref={containerRef} className="flex whitespace-nowrap gap-3 transition-transform w-[max-content]" style={{ transform: `translateX(-${translate}px)` }}>
                     {categories.map(category => (
-                        <Button 
-                            key={category} 
-                            variant={selectedCategory === category ? 'dark' : 'default' } 
+                        <Button
+                            key={category}
+                            variant={selectedCategory === category ? 'dark' : 'default'}
                             className="py-1 px-3 rounded-lg whitespace-nowrap"
                             onClick={() => onSelect(category)}
                         >
@@ -22,6 +30,36 @@ function CategoryPills({ categories, selectedCategory, onSelect }: CategoryPills
                         </Button>
                     ))}
                 </div>
+                {isLeftVisible && <div className="absolute left-0 top-1/2 -translate-y-1/2 bg-gradient-to-r from-white from-50% to-transparent w-24 h-full flex justify-end">
+                    <Button variant='ghost' size='icon' className="h-full aspect-square w-auto p-1.5" onClick={() => {
+                        setTranslate(translate => {
+                            const newTranslate = translate - TRANSLATE_AMOUNT
+                            if (newTranslate <= 0) {
+                                return 0
+                            } else {
+                                return newTranslate
+                            }
+                        })
+                    }}>
+                        <ChevronLeft />
+                    </Button>
+                </div>}
+                {isRightVisible && <div className="absolute left-0 top-1/2 -translate-y-1/2 bg-gradient-to-r from-white from-50% to-transparent w-24 h-full">
+                    <Button variant='ghost' size='icon' className="h-full aspect-square w-auto p-1.5" onClick={() => {
+                        setTranslate(translate => {
+                            if (containerRef.current == null) return translate
+                            const newTranslate = translate + TRANSLATE_AMOUNT
+                            const edge = containerRef.current.scrollWidth
+                            const width = containerRef.current.clientWidth
+                            if (newTranslate + width >= edge)
+                                return edge - width
+                            else
+                                return newTranslate
+                        })
+                    }}>
+                        <ChevronRight />
+                    </Button>
+                </div>}
             </div>
         </>
     )
