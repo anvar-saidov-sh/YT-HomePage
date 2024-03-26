5e6a218e0b9e36929566139fd58b6dbd8ed76776
anvarsaidovsh <bananoanimation12@gmail.com>
2024-03-26 23:31:50 +0500

    working on category

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,38 @@
+import { useState } from "react"
+import CategoryPills  from "./components/CategoryPills"
+import { categories } from "./data/home"
+import {videos} from './data/home'
+import { PageHeader } from "./layouts/PageHeader"
+import { VideoGridItem } from "./components/VideoGridItem"
+import { Sidebar } from "./layouts/Sidebar"
+import { SidebarProvider } from "./contexts/SidebarContext"
+import { JSX } from "react/jsx-runtime"
 
-import { useState } from 'react'
-import './App.css'
-import CategoryPills from './components/CategoryPills'
-import { categories } from './data/home'
-import PageHeader from './layouts/PageHeader'
-
-
-function App() {
+export default function App() {
   const [selectedCategory, setSelectedCategory] = useState(categories[0])
+
   return (
-    <>
-      <div className='max-h-screen flex flex-col'>
+    <SidebarProvider>
+      <div className="max-h-screen flex flex-col">
         <PageHeader />
-        <div className='grid grid-cols-auto-[auto, 1fr] flex-grow-1 overflow-auto'>
-          <div>Sidebar</div>
-          <div className='overflow-x-hidden px-8 pb-4'>
-            <div className='sticky top-0 bg-white z-10 pb-4'>
-              <CategoryPills categories={categories} selectedCategory={selectedCategory} onSelect={setSelectedCategory} />
+        <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
+          <Sidebar />
+          <div className="overflow-x-hidden px-8 pb-4">
+            <div className="sticky top-0 bg-white z-10 pb-4">
+              <CategoryPills
+                categories={categories}
+                selectedCategory={selectedCategory}
+                onSelect={setSelectedCategory}
+              />
+            </div>
+            <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(300px,1fr))]">
+              {videos.map((video: JSX.IntrinsicAttributes & { id: string; title: string; channel: { id: string; name: string; profileUrl: string }; views: number; postedAt: Date; duration: number; thumbnailUrl: string; videoUrl: string }) => (
+                <VideoGridItem key={video.id} {...video} />
+              ))}
             </div>
           </div>
         </div>
       </div>
-    </>
+    </SidebarProvider>
   )
 }
-
-export default App
